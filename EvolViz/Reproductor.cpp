9553f7f4c1d04d33140a76fde252916af5a60378
Andrzej Fiedukowicz <fiedukow@gmail.com>
2014-01-25 23:01:11 +0100

    Reproductor factory implemented.

--- EvolViz/Reproductor.cpp
+++ EvolViz/Reproductor.cpp
@@ -2,7 +2,19 @@
 namespace model {
 
 ReproductorPtr Reproductor::produce(const common::ReproductionOptions& options) {
-	return ReproductorPtr(); // FIXME
+	return ReproductorPtr(new ReproductorImpl(options.rate));
+}
+
+Reproductor::Reproductor(const double rate)
+	: rate_(rate) {
+}
+
+ReproductorImpl::ReproductorImpl(const double rate)
+	: Reproductor(rate) {
+}
+
+void ReproductorImpl::operator()(Population& population) const {
+	// FIXME implement this
 }
 
 } // namespace model
--- EvolViz/Reproductor.h
+++ EvolViz/Reproductor.h
@@ -8,8 +8,18 @@ struct Population;
 
 class Reproductor {
 public:
-	virtual void operator()(Population& population) = 0;
+	Reproductor(const double rate);
+	virtual void operator()(Population& population) const = 0;
 	static std::shared_ptr<Reproductor> produce(const common::ReproductionOptions& options);
+
+protected:
+	const double rate_;
+};
+
+class ReproductorImpl : public Reproductor {
+public:
+	ReproductorImpl(const double rate);
+	virtual void operator()(Population& population) const override;
 };
 
 typedef std::shared_ptr<Reproductor> ReproductorPtr;
