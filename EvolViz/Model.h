bf2da00460308e5cf53f758ff12bb56819ce9080
Andrzej Fiedukowicz <fiedukow@gmail.com>
2014-01-25 14:58:02 +0100

    Model interface stubs + Options structures.

--- EvolViz/Model.h
+++ EvolViz/Model.h
@@ -1,10 +1,30 @@
 #pragma once
+#include <chrono>
+#include "ModelOptions.h"
+
+namespace model {
+
 class Model
 {
 public:
 	Model();
 	~Model();
 
+	void doStep();
+	void doGeneration();
+	void doGenerations(unsigned int generations, std::chrono::milliseconds interval = std::chrono::milliseconds(0));
+	
+	void setFitnessFunction(const std::string& formula);
+	void setInitializationOptions(const common::InitializationOptions& options);
+	void setMutationOptionsX(const common::MutationOptions& options);
+	void setMutationOptionsY(const common::MutationOptions& options);
+	void setCrossOverOptions(const common::CrossOverOptions& options);
+	void setRangeOptions(const common::RangeAlignmentOptions& type);
+	void setSelectionType(const common::SelectionType& type);	
+	void setPopulationSize(const unsigned int& size);
+	void setGoalValue(const double& goal);
+
 	void operator()();
 };
 
+} // namespace model
---
+++ EvolViz/ModelOptions.h
@@ -0,0 +1,64 @@
+#pragma once
+namespace common {
+	/* Randomness options */
+	struct UniversalRandomOptions {
+		double min;
+		double max;
+	};
+	struct GaussRandomOptions {
+		double expected;
+		double variation;
+	};
+
+	/* Initialization options */
+	struct InitializationOptions {};
+	struct PointInitialization : public InitializationOptions {
+		double x;
+		double y;
+	};
+	struct RandomInitialization : public InitializationOptions {};
+
+	/* Mutation options */
+	struct MutationOptions {
+		double mutation_rate;
+	};
+	struct UniversalRandomMutation : public MutationOptions, public UniversalRandomOptions {};
+	struct GaussRandomMutation : public MutationOptions, public GaussRandomOptions {};
+	struct ConstMutation : public MutationOptions {
+		double value;
+	};
+
+	/* Crossover options */
+	struct CrossOverOptions {
+		double cross_over_factor;
+	};
+	struct ConstAvgCrossOver : public CrossOverOptions {
+		double x_weight;
+		double y_weight;
+	};
+	struct UniversalRandomAvgCrossOver : public CrossOverOptions {
+		UniversalRandomOptions x;
+		UniversalRandomOptions y;
+	};
+	struct GaussRandomAvgCrossOver : public CrossOverOptions {
+		GaussRandomOptions x;
+		GaussRandomOptions y;
+	};
+	struct UniversalRandomFixedCrossOver : public CrossOverOptions {};
+
+	/* Alignment options */
+	struct RangeAlignmentOptions {
+		double x_min;
+		double x_max;
+		double y_min;
+		double y_max;
+	};
+	struct RollingRangeAlignment : public RangeAlignmentOptions {};
+	struct MirroringRangeAlignment : public RangeAlignmentOptions {};
+	struct UniversalRandomReinitializationAlignment : public RangeAlignmentOptions {};
+
+	/* Selection options */
+	struct SelectionType {
+		enum type { ELITE, LOTERY, TOURNAMENT, ROULETTE };
+	};
+} // namespace common
